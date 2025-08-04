709db1eb62feb8e6b0b21e2de7fd46b205de8b6e
UTKRISHT-ICS52 <codewithutkrisht@gmail.com>
2025-08-04 23:00:08 +0530

    Changes by utkrisht

--- code.cpp
+++ code.cpp
@@ -269,7 +269,6 @@ int sumN(int n){
 }
 
 
-*/
 
 #include<iostream>
 using namespace std; 
@@ -286,6 +285,30 @@ return fact;
         cout << factorialN(4) <<endl;
          cout << factorialN(5) <<endl;
          return 0; 
+}
+
+*/
+
+#include<iostream>
+using namespace std;
+int decToBinary(int decNum){
+    int ans =0, pow =1;
+
+    while(decNum>0){
+        int rem = decNum %2;
+        decNum /=2;
+
+        ans +=(rem * pow);
+        pow *=10;
+    }
+    return ans;  // binary form 
+}
+int main (){
+    int decNum = 100;
+   // for(int i =1; i<=10; i++)
+        cout<< decToBinary(decNum) <<endl;
+    
+    return 0;
 }
 
 
