01f00585040ce2c471a9e35b6264e3de25d10434
UTKRISHT-ICS52 <codewithutkrisht@gmail.com>
2025-08-28 16:31:46 +0530

    Today tASK DONE

--- Oops.cpp
+++ Oops.cpp
@@ -1,5 +1,5 @@
 // Lab 1st Program 
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 class age
 {
@@ -21,4 +21,62 @@ age age_insert(20);
 age_default.disp();
 age_insert.disp();
 return 0;
+}
+
+
+
+// Lab 1st Program 
+#include<vector>
+using namespace std;
+class age
+{
+public:
+float num;
+age (){
+num=0;
+}
+age (int a){
+num=a;
+}
+void disp(){
+cout<<num<<endl;
+}
+};
+int main(){
+age age_default;
+age age_insert(20);
+age_default.disp();
+age_insert.disp();
+return 0;
+}
+*/
+#include<iostream>
+using namespace std;
+class Email{
+    private:
+    string password;
+
+    protected:
+    string email;
+
+    public:
+    void emailInput(){
+        string email,password;
+        cin>>email;
+        cin>>password;
+        this->email=email;
+        this->password=password;
+    }
+    void emailDisplay(){
+        cout<<"Email is : "<<this->email<<endl;
+    }
+
+};
+
+int main(){
+    Email email1;
+    cout<<"Enter Email/Password"<<endl;
+    email1.emailInput();
+    email1.emailDisplay();
+    return 0;
 }
